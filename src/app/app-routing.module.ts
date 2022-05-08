89f3bab56e86a8f4c336352bbab12774b31b8241
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-08 05:27:45 -0300

    :sparkles: feat: create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewTransitionComponent } from './components/new-transition/new-transition.component';
+import { TransitionsListComponent } from './components/transitions-list/transitions-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new-transition', component: NewTransitionComponent },
+  { path: 'transitions', component: TransitionsListComponent },
+  { path: '', redirectTo: 'transitions', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
