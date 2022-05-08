12c446f3abce5a655926ee6a1a276cad55350c76
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-08 05:37:46 -0300

    :sparkles: feat: create simple table to render transitions

--- src/app/components/transitions-list/transitions-list.component.ts
+++ src/app/components/transitions-list/transitions-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transitions-list.component.css']
 })
 export class TransitionsListComponent implements OnInit {
+  transitions: any;
 
   constructor() { }
 
