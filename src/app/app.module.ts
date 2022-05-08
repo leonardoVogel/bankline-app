49150e43fbd09b3eca3045e292526aed71fcfac0
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-08 05:29:00 -0300

    :recycle: refactor: remove unnecessary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransitionsListComponent } from './components/transitions-list/transitions-list.component';
+import { NewTransitionComponent } from './components/new-transition/new-transition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransitionsListComponent,
+    NewTransitionComponent
   ],
   imports: [
     BrowserModule,
